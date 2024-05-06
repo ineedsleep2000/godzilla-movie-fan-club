aa51ebf795a85c988cb3f1e6c698c6e09083cd1c
Brian <turtle40965@gmail.com>
2024-05-06 11:18:21 -0400

    added mouseover, mouseout, click, and submit events. added background and moved around items with css

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-start up db.json file by going into terminal and typing=> npm install -g json-server => json-server --watch db.json
+start up db.json file by going into terminal and typing=>
+npm install -g json-server => json-server --watch db.json
+
 # phase-1-final-project
--- src/index.js
+++ src/index.js
@@ -7,15 +7,58 @@ fetch("http://localhost:3000/movies")
   .then((moviesArr) => moviesArr.forEach((movie) => renderGodzilla(movie)));
 
 /////////////////////
-//render functions
+//Global Variables
+/////////////////////
+const selectedMovieImg = document.querySelector("#selected-img");
+const selectedMovieDescription = document.querySelector(
+  "#selected-description"
+);
+const movieReviewer = document.querySelector("#movieReviewer");
+
+/////////////////////
+//render function, along with movie bar events
 /////////////////////
 
 function renderGodzilla(movie) {
   const godzillaMovies = document.querySelector("#Godzilla-movies");
-  const godzillaImages = document.createElement("Img");
+  const godzillaImages = document.createElement("img");
   godzillaImages.src = movie.imageUrl;
+  //movie bar mouseover event
+  godzillaImages.addEventListener("mouseover", (e) => {
+    godzillaImages.style.transform = "scale(1.5)";
+    //movie select, click event
+    godzillaImages.addEventListener("click", (e) => {
+      selectedMovieImg.src = movie.imageUrl;
+      selectedMovieImg.style.transform = "scale(2.25)";
 
-  //godzillaImages.addEventListener("mouseover", (e) => {});
+      const selectedMovieYear = document.querySelector("#movie-year");
+      const selectedMovieProducer = document.querySelector("#movie-producer");
+      const selectedMovieDirector = document.querySelector("#movie-director");
+      const selectedMovieMonster = document.querySelector("#movie-monster");
+      selectedMovieYear.textContent = "Year of Release: " + movie.year;
+      selectedMovieProducer.textContent = "Movie Producer: " + movie.producer;
+      selectedMovieDirector.textContent = "Movie Director: " + movie.director;
+      selectedMovieMonster.textContent =
+        "Monster Appearances: " + movie.monsters;
+    });
+    //movie bar mouseout event
+    godzillaImages.addEventListener("mouseout", (e) => {
+      godzillaImages.style.transform = "scale(1.0)";
+    });
+  });
 
   godzillaMovies.append(godzillaImages);
 }
+
+///////////////
+//submit event
+//////////////
+
+movieReviewer.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const reviewInput = document.querySelector("#review-input");
+  const review = document.querySelector("#review");
+  review.textContent = reviewInput.value;
+  console.log(reviewInput);
+  console.log(review);
+});
